31abd2b226a7f28057324d4e3b9e18f09d06d0c4
Connor Kelly <connor.r.kelly@gmail.com>
2024-02-11 21:05:36 -0600

    Provision variables into caddy usage pool
    Signed-off-by: Connor Kelly <connor.r.kelly@gmail.com>

--- app.go
+++ app.go
@@ -25,3 +25,31 @@ func (TSApp) CaddyModule() caddy.ModuleInfo {
 		New: func() caddy.Module { return new(TSApp) },
 	}
 }
+
+func (t *TSApp) Provision(ctx caddy.Context) error {
+	app.LoadOrStore(authUsageKey, t.DefaultAuthKey)
+	app.LoadOrStore(ephemeralKey, t.Ephemeral)
+
+	for _, svr := range t.Servers {
+		app.LoadOrStore(svr.name, svr)
+	}
+	return nil
+}
+
+func (t *TSApp) Validate() error {
+	if t.DefaultAuthKey == "" {
+		return errors.New("auth_key must be set")
+	}
+	return nil
+}
+
+func (t *TSApp) Start() error {
+	return nil
+}
+
+func (t *TSApp) Stop() error {
+	return nil
+}
+
+// var _ caddyfile.Unmarshaler = (*TSApp)(nil)
+var _ caddy.App = (*TSApp)(nil)
